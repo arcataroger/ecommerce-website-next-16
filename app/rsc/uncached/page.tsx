445075b014efbd717fd4f92154381f3bc4fdb018
Roger Tuan <r.tuan@datocms.com>
2025-12-02 16:16:40 -0800

    Basic time-based RSC invalidation

---
+++ app/rsc/uncached/page.tsx
@@ -0,0 +1,44 @@
+import { graphql } from "gql.tada";
+import { executeQuery } from "@datocms/cda-client";
+
+export default async function Test() {
+  const HomeQuery = graphql(`
+    query MyQuery {
+      home {
+        id
+        label
+        slug
+        sections {
+          ... on CollectionCardShowcaseSectionRecord {
+            id
+            title
+            pretitle
+            direction
+            description
+          }
+        }
+      }
+    }
+  `);
+
+  const data = await executeQuery(HomeQuery, {
+    token: process.env.DATOCMS_API_TOKEN!,
+/*    fetchFn: (input, init) =>
+      fetch(input, {
+        ...init,
+        cache: "force-cache",
+        next: {
+          revalidate: 1,
+        },
+      }),*/
+  });
+
+  return (
+    <>
+      <h1>Basic Fetch Test</h1>
+      <pre>
+        <code>{JSON.stringify(data, null, 2)}</code>
+      </pre>
+    </>
+  );
+}
